16940bd25b2f88ffc999204d4dcdf1a0d7f67d35
Maria Ramos <113792739+Athenas223@users.noreply.github.com>
2023-07-24 13:35:13 -0300

    Update AddPointsMEDC.cs

--- AddPointsMEDC.cs
+++ AddPointsMEDC.cs
@@ -13,16 +13,13 @@ public class AddPointsMEDC : MonoBehaviour
 
     public void AddFourPoints()
     {
-        // Verifica se o texto no InputField é um número inteiro válido
         if (int.TryParse(MEDInput.text, out int currentValue))
         {
-            // Adiciona cinco pontos ao valor atual e atualiza o InputField com o novo valor
             int newPoints = currentValue + 4;
             MEDInput.text = newPoints.ToString();
         }
         else
         {
-            // Se o texto não for um número inteiro válido, assume o valor inicial de 5
             MEDInput.text = "4";
         }
     }
