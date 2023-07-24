d013a5f79f5f3b62d226b318634ea6718db5b4e2
Maria Ramos <113792739+Athenas223@users.noreply.github.com>
2023-07-24 13:36:37 -0300

    Update SubPointsHIGHC.cs

--- SubPointsHIGHC.cs
+++ SubPointsHIGHC.cs
@@ -13,16 +13,13 @@ public class SubPointsHIGHC : MonoBehaviour
 
     public void SubFivePoints()
     {
-        // Verifica se o texto no InputField é um número inteiro válido
         if (int.TryParse(HIGHInput.text, out int currentValue))
         {
-            // Adiciona cinco pontos ao valor atual e atualiza o InputField com o novo valor
             int newPoints = currentValue - 5;
             HIGHInput.text = newPoints.ToString();
         }
         else
         {
-            // Se o texto não for um número inteiro válido, assume o valor inicial de 5
             HIGHInput.text = "5";
         }
     }
